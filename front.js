c758772d49180d8de85f069d078e45317e1ee324
Greg Banks <gbanks@sgi.com>
2014-03-23 21:40:12 -0700

    Redesigned front page.
    Now have the obligatory 3 columns with big green buttons.
    
    Also made the header shorter to reduce wasted space.

--- front.js
+++ front.js
@@ -1,27 +1,6 @@
-
-var pages;
-var delay = 3000;
-
-function show_next()
-{
-    var next = pages.next();
-    if (next.length) {
-	pages.hide();
-	pages = next;
-	pages.show();
-	window.setTimeout(show_next, delay);
-    }
-}
-
 $(document).ready(function() {
     /* On FF the formatting screws up if this button
      * is hidden by default and then shown in JS */
     if (navigator.userAgent.indexOf('Ubuntu;') < 0)
-	$('#front #curious #ubuntu').hide();
-
-    pages = $('#front .cloud');
-    pages.css('margin-left', (Math.max(0, ($(window).width() - pages.first().outerWidth())) / 2) + "px");
-    pages = pages.first();
-    pages.show();
-    window.setTimeout(show_next, delay);
+	$('#front #ubuntu').hide();
 });
